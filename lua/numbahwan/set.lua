e96fbf43da529851fd0fcd0c6ff2de2b0bdf0243
Aldrich Choi <aldrich.choi@woven-planet.global>
2024-07-03 13:35:22 +0900

    Initial commit

---
+++ lua/numbahwan/set.lua
@@ -0,0 +1,38 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+-- vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Save undo history
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 10
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.hlsearch = true
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+-- vim.opt.colorcolumn = "80"
