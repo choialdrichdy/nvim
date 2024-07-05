8fcfa2f62f19f38bbbbd6e582459439ca9003d52
Aldrich Choi <choi.aldrich@gmail.com>
2024-07-06 01:08:07 +0900

    Adds tmux nvim integration and Maps go template to html parser

--- lua/numbahwan/init.lua
+++ lua/numbahwan/init.lua
@@ -1,14 +1,15 @@
 require("numbahwan.remap")
 require("numbahwan.set")
 require("numbahwan.autocmd")
+require("numbahwan.ft")
 -- Comment if not in WSL
 require("numbahwan.wsl")
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
 end
 vim.opt.rtp:prepend(lazypath)
 
