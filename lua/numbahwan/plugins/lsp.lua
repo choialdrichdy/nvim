feeb3d502e060ae84ccaf06f63187d9fdc68b166
Aldrich Choi <choi.aldrich@gmail.com>
2024-07-07 15:43:39 +0900

    Adds htmx lsp

--- lua/numbahwan/plugins/lsp.lua
+++ lua/numbahwan/plugins/lsp.lua
@@ -159,6 +159,7 @@ return {
 			pyright = {},
 			tailwindcss = {},
 			html = {},
+			htmx = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
